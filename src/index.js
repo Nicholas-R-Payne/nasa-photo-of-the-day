8b9d5fb62a6351cf762a7ccce215e198a1c8a491
DarkedgeTH <darkedgeth@gmail.com>
2022-02-17 14:47:14 -0500

    imported and rendered ThemeProvider

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+// import ThemeProvider and theme
+import {ThemeProvider} from 'styled-components';
+import theme from './Theme'
+// render ThemeProvider
+ReactDOM.render(
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>
+    ,
+    document.getElementById("root")
+    );
